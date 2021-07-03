46978fd57483145ed42d4d14d89848a6d65ad74d
Dong-kyu-Lee <stvboy00@gmail.com>
2021-07-03 22:03:11 +0900

    until 19강

--- New Unity Project/Assets/Script/Title/StartBtn.cs
+++ New Unity Project/Assets/Script/Title/StartBtn.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Text나 Botton같은 UGUI의 컴포넌트를 사용할 수 있다.
+using UnityEngine.UI;
+//Ipointer 이벤트 등록
+using UnityEngine.EventSystems;
+//Scene 넘기는 일
+using UnityEngine.SceneManagement;
 
-public class StartBtn : MonoBehaviour
+public class StartBtn : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Play"); // Scene 로드
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+    
     void Start()
     {
         
